0b25b35b1ad1301f8ee9211aecce12edc68ac191
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-04-26 17:14:20 +0200

    Move public folder to shared

--- apps/tool-finder/vite.config.ts
+++ apps/tool-finder/vite.config.ts
@@ -2,8 +2,8 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  publicDir: "../shared/public",
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "../shared"),
