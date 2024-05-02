d5cf9e1b58c013941a892fb33f0216bb12dadaeb
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-02 11:48:36 +0200

    Fix tool-finder jest (reuse much of dito)

--- packages/tool-finder/jest.config.ts
+++ packages/tool-finder/jest.config.ts
@@ -1,14 +1,12 @@
 import type { Config } from "@jest/types";
 
 const config: Config.InitialOptions = {
-  modulePaths: ["<rootDir>"],
-  moduleFileExtensions: ["js", "ts", "tsx", "json"],
-  transform: {
-    "^.+\\.(j|)tsx?$": "ts-jest",
-  },
+  preset: "ts-jest",
   testEnvironment: "jsdom",
+  moduleDirectories: ["node_modules", "<rootDir>/src"],
   testPathIgnorePatterns: ["<rootDir>/test/e2e", "<rootDir>/test/a11y"],
-  collectCoverageFrom: ["src/**", "!**/*.d.ts"],
+  setupFilesAfterEnv: ["<rootDir>/test/jest.setup.ts"],
+  collectCoverageFrom: ["src/**/*.{ts,tsx,js,jsx}", "!**/*.d.ts"],
 };
 
 export default config;
