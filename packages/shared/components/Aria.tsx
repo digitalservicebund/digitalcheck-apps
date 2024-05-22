89287b83c535e5e48afd40e05cc997e5ac92d537
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-22 09:57:11 +0200

    Revert "Test changes in shared"
    This reverts commit a04fc32854e36133ce3c6e9583f7147ccd814ede.

--- packages/shared/components/Aria.tsx
+++ packages/shared/components/Aria.tsx
@@ -3,7 +3,7 @@ export const A11Y_MESSAGE_NEW_WINDOW: string = "a11y-message--new-window";
 export function Aria() {
   return (
     <span aria-hidden="true" className="hidden" id={A11Y_MESSAGE_NEW_WINDOW}>
-      (Öffnet in neuem Fenster)
+      (öffnet in neuem Fenster)
     </span>
   );
 }
