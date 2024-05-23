91578868887496f875d0699fe3199f7e7ec42fe3
sarah strozynski <24942058+sarahstrozynski@users.noreply.github.com>
2024-05-23 10:58:57 +0200

    Update wording BetaBanner.tsx

--- packages/shared/components/BetaBanner.tsx
+++ packages/shared/components/BetaBanner.tsx
@@ -7,8 +7,7 @@ const BetaBanner = () => {
     <Background backgroundColor="yellow" paddingTop="16" paddingBottom="16">
       <Container paddingTop="0" paddingBottom="0">
         <RichText
-          markdown={`Der Werkzeugfinder befindet sich im Aufbau. 
-          Inhalte werden noch angepasst und die Funktionalität kann zeitweise eingeschränkt sein.`}
+          markdown={`Der Werkzeugfinder befindet sich im Aufbau und wird auf Basis Ihrer Rückmeldung weiterentwickelt. Inhalte werden regelmäßig angepasst und die Funktionalität kann zeitweise eingeschränkt sein.`}
         />
       </Container>
     </Background>
