62fce9fa8079ae4f467e2ddd55f0e7b95851186e
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-27 15:05:33 +0200

    Update noopTracking.tsx help string

--- packages/shared/services/noopTracking.tsx
+++ packages/shared/services/noopTracking.tsx
@@ -3,7 +3,7 @@ import SharedTracking from "./sharedTracking.tsx";
 class NoopTracking implements SharedTracking {
   trackButtonClick(id: string | undefined, href: string | undefined) {
     console.debug(
-      `Button click not tracked for id="${id}" and href="${href}". Inject a tracking service into the context to enable tracking.`,
+      `Button click not tracked for id="${id}" and href="${href}". Provide a tracking service in the TrackingContext to enable tracking.`,
     );
   }
 }
