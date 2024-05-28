d3bae9843da1a01001c7a2a6988494aa5e5d14ed
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-28 14:25:41 +0200

    Add landing header

--- packages/dito/app/resources/content.ts
+++ packages/dito/app/resources/content.ts
@@ -5,3 +5,8 @@ export const header = {
     number: "0151/40 76 78 39",
   },
 };
+
+export const landing = {
+  title: "Digitaltaugliche Regelungsvorhaben erarbeiten",
+  subtitle: "Heute haben fast alle Regelungsvorhaben ...",
+};
