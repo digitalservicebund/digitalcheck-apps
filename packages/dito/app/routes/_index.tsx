73e997b36c359fed7ec0784f3202c191a8398b12
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-29 12:47:37 +0200

    Improve landing header styling

--- packages/dito/app/routes/_index.tsx
+++ packages/dito/app/routes/_index.tsx
@@ -15,10 +15,10 @@ export const meta: MetaFunction = () => {
 export default function Index() {
   return (
     <main className="">
-      <div className="bg-blue-100 py-48">
-        <div className="container ds-stack-16">
+      <div className="bg-blue-100">
+        <div className="container ds-stack-16 pt-64 pb-48">
           <h1>{landing.title}</h1>
-          <p>{landing.subtitle}</p>
+          <p className="ds-subhead">{landing.subtitle}</p>
         </div>
       </div>
       <div className="container ds-stack-16 pt-40 pb-48">
