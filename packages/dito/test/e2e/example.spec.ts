a29c13f4271c88e6178e7c0d52c6706fa26ad63a
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-29 12:51:33 +0200

    Fix dito test

--- packages/dito/test/e2e/example.spec.ts
+++ packages/dito/test/e2e/example.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from "@playwright/test";
 test.describe("basic example test", () => {
   test("has title", async ({ page }) => {
     await page.goto("/");
-    await expect(page).toHaveTitle("Dito");
+    await expect(page).toHaveTitle("Digitalcheck");
   });
 
   test("shows hello message", async ({ page }) => {
