b6a4fb63d7352876b8c9a347084170f51501dcd6
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-30 22:10:45 +0200

    Fix bubbles responsiveness

--- packages/dito/app/components/NumberedList.tsx
+++ packages/dito/app/components/NumberedList.tsx
@@ -22,7 +22,7 @@ export default function NumberedList({
       <ol className="list-none list-outside ds-stack-32 marker:border-2 marker:border-blue-600">
         {listItems.map((item, index) => (
           <li key={item.title} className="flex space-x-16">
-            <div className="border border-gray-400 rounded-full w-40 h-40 flex justify-center items-center">
+            <div className="border border-gray-400 rounded-full w-40 h-40 flex-none flex justify-center items-center">
               {index + 1}
             </div>
             <Box
