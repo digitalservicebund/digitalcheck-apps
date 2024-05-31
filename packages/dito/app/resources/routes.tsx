3b87446dfcabc546c63087493e78afb1c5a00bc3
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-05-31 14:42:21 +0200

    Add breadcrumbs to layout

--- packages/dito/app/resources/routes.tsx
+++ packages/dito/app/resources/routes.tsx
@@ -1,3 +1,28 @@
+import HomeOutlined from "@digitalservicebund/icons/HomeOutlined";
+
+export const PATH_LANDING: string = "/";
 export const PATH_IMPRINT: string = "/impressum";
 export const PATH_PRIVACY: string = "/datenschutz";
 export const PATH_A11Y: string = "/barrierefreiheit";
+
+export const ROUTES = [
+  {
+    url: PATH_LANDING,
+    Icon: HomeOutlined,
+  },
+  {
+    url: PATH_IMPRINT,
+    title: "Impressum",
+    parent: PATH_LANDING,
+  },
+  {
+    url: PATH_A11Y,
+    title: "Barrierefreiheit",
+    parent: PATH_LANDING,
+  },
+  {
+    url: PATH_PRIVACY,
+    title: "Datenschutzerklärung",
+    parent: PATH_LANDING,
+  },
+];
