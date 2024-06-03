2c3cd7e0f4cb9873685f7ddd0c8ca78925181c49
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-03 19:07:39 +0200

    Make Question accept wrapper props

--- packages/shared/components/Question.tsx
+++ packages/shared/components/Question.tsx
@@ -1,5 +1,11 @@
+import classNames from "classnames";
+import { PropsWithChildren } from "react";
+import { CommonWrapperProps } from "./CommonWrapperProps";
 import RadioGroup, { RadioGroupProps } from "./RadioGroup";
 import Select, { SelectProps } from "./Select";
+
+const DEFAULT_PADDING_TOP = "0";
+const DEFAULT_PADDING_BOTTOM = "80";
 
 export type QuestionProps = {
   heading: string;
@@ -7,17 +13,33 @@ export type QuestionProps = {
   description: string;
   select?: SelectProps;
   radio?: RadioGroupProps;
-};
+  additionalClassNames?: string;
+} & PropsWithChildren<CommonWrapperProps>;
 
 export default function Question({
+  paddingTop = "default",
+  paddingBottom = "default",
+  backgroundColor = "default",
   heading,
   label,
   description,
   select,
   radio,
+  additionalClassNames,
 }: QuestionProps) {
+  let cssClasses = additionalClassNames ?? "";
+  cssClasses = classNames(
+    cssClasses,
+    "container",
+    `!pt-${paddingTop === "default" ? DEFAULT_PADDING_TOP : paddingTop}`,
+    `!pb-${
+      paddingBottom === "default" ? DEFAULT_PADDING_BOTTOM : paddingBottom
+    }`,
+    backgroundColor !== "default" && "text-black",
+  );
+
   return (
-    <fieldset className="container pt-0 pb-80">
+    <fieldset className={cssClasses}>
       <legend className="pb-16 ds-stack-8">
         {label && <p className="ds-label-02-reg">{label}</p>}
         <h2>{heading}</h2>
