599393fdac86a442dacf62276c6441788e0e20f0
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-05 16:19:36 +0200

    Update ButtonContainer.tsx

--- packages/shared/components/ButtonContainer.tsx
+++ packages/shared/components/ButtonContainer.tsx
@@ -2,15 +2,24 @@ import classNames from "classnames";
 import type { PropsWithChildren } from "react";
 
 type ButtonContainerProps = PropsWithChildren & {
-  reverseOrder?: boolean;
+  readonly reverseOrder?: boolean;
+  readonly className?: string;
 };
 
-const ButtonContainer = ({ children, reverseOrder }: ButtonContainerProps) => {
+const ButtonContainer = ({
+  children,
+  reverseOrder,
+  className,
+}: ButtonContainerProps) => {
   return (
     <div
-      className={classNames("flex flex-wrap gap-24", {
-        "flex-wrap-reverse": reverseOrder,
-      })}
+      className={classNames(
+        "flex flex-wrap gap-24",
+        {
+          "flex-wrap-reverse": reverseOrder,
+        },
+        className,
+      )}
     >
       {children}
     </div>
