6102f039d86f0dd06e3269ba763142cbace4093d
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-11 19:22:19 +0200

    Add tests for question flow

--- packages/dito/tests/e2e/preCheck.spec.ts
+++ packages/dito/tests/e2e/preCheck.spec.ts
@@ -1,5 +1,147 @@
 import { expect, test } from "@playwright/test";
+import { preCheck } from "resources/content";
 import * as staticRoutes from "resources/staticRoutes";
+
+test.describe("test questions form", () => {
+  test("all answer options are submittable", async ({ page }) => {
+    await page.goto(staticRoutes.PATH_PRECHECK);
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Nein").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Ich bin unsicher").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Nein").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Ich bin unsicher").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page).toHaveURL(staticRoutes.PATH_RESULT);
+  });
+
+  test("clicking through questions works", async ({ page }) => {
+    await page.goto(staticRoutes.PATH_PRECHECK);
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    for (let i = 0; i < 5; i++) {
+      // check that the page shows correct question
+      await expect(page).toHaveURL(preCheck.questions[i].url);
+      await expect(page.getByLabel("navigation")).toContainText(
+        preCheck.questions[i].title,
+      );
+      await expect(page.getByRole("main")).toContainText(`Frage ${i + 1}`);
+      await expect(page.getByRole("main")).toContainText(
+        preCheck.questions[i].question,
+      );
+      const hint = preCheck.questions[i].hint;
+      if (hint) {
+        await expect(page.getByRole("main")).toContainText(hint.title);
+      }
+      await page.getByLabel("Ja").click();
+      await page.getByRole("button", { name: "Übernehmen" }).click();
+    }
+    await expect(page).toHaveURL(staticRoutes.PATH_RESULT);
+  });
+
+  test("cant submit form without answers", async ({ page }) => {
+    await page.goto(preCheck.questions[0].url);
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page).toHaveURL(preCheck.questions[0].url);
+    await expect(page.getByRole("main")).toContainText(
+      "Bitte wählen Sie eine Option aus.",
+    );
+  });
+
+  test("back button works", async ({ page }) => {
+    await page.goto(preCheck.questions[0].url);
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page).toHaveURL(staticRoutes.PATH_PRECHECK);
+    await page.goto(preCheck.questions[0].url);
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page).toHaveURL(preCheck.questions[1].url);
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page).toHaveURL(preCheck.questions[0].url);
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page).toHaveURL(staticRoutes.PATH_PRECHECK);
+  });
+
+  test("answers are saved and loaded from cookie and persisted across navigations and submissions", async ({
+    page,
+  }) => {
+    // was a bit of a hassle to get the cookie and react-hook-form to work together with useEffect
+    // that's why the test is a bit more extensive than it could be
+    await page.goto(staticRoutes.PATH_PRECHECK);
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Nein").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Ich bin unsicher").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Ja").click();
+    await page.reload();
+    await expect(page.getByLabel("Ja")).not.toBeChecked();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page.getByLabel("Ich bin unsicher")).toBeChecked();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page.getByLabel("Nein")).toBeChecked();
+    await page.goto(preCheck.questions[0].url);
+    await expect(page.getByLabel("Ja")).toBeChecked();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page.getByLabel("Nein")).toBeChecked();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page.getByLabel("Ich bin unsicher")).toBeChecked();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page.getByLabel("Nein")).toBeChecked();
+    await page.reload();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(page.getByLabel("Ja")).toBeChecked();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page.getByLabel("Nein")).toBeChecked();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page.getByLabel("Ich bin unsicher")).toBeChecked();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(page.getByLabel("Ja")).not.toBeChecked();
+    await expect(page.getByLabel("Nein")).not.toBeChecked();
+    await expect(page.getByLabel("Ich bin unsicher")).not.toBeChecked();
+  });
+
+  test("redirect to first unanswered question", async ({ page }) => {
+    await page.goto(staticRoutes.PATH_PRECHECK);
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByLabel("Nein").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.goto(preCheck.questions[4].url);
+    await expect(page).toHaveURL(preCheck.questions[2].url);
+    await page.getByLabel("Nein").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.goto(preCheck.questions[4].url);
+    await expect(page).toHaveURL(preCheck.questions[3].url);
+  });
+
+  // test cookie is reset
+  test("answers / cookie is reset on preCheck start", async ({ page }) => {
+    await page.goto(preCheck.questions[0].url);
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    await expect(page.getByLabel("Ja")).not.toBeChecked();
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    await page.goto(preCheck.questions[0].url);
+    await expect(page.getByLabel("Ja")).not.toBeChecked();
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.goto(staticRoutes.PATH_PRECHECK);
+    await page.getByRole("link", { name: "Einschätzen" }).click();
+    await expect(page.getByLabel("Ja")).not.toBeChecked();
+  });
+});
 
 test.describe("test result page", () => {
   test("happy path leads to positive result", async ({ page }) => {
