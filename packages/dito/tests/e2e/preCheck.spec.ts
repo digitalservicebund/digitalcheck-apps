87195f03814c5de618e89a1ed21edc9f8c773c2b
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-11 23:48:33 +0200

    Add question navigation tests

--- packages/dito/tests/e2e/preCheck.spec.ts
+++ packages/dito/tests/e2e/preCheck.spec.ts
@@ -147,6 +147,134 @@ test.describe("test questions form", () => {
   });
 });
 
+test.describe("test question navigation", () => {
+  test("navigation leads to correct pages", async ({ page }) => {
+    await page.goto(PATH_PRECHECK);
+    await page.getByRole("link", { name: "Digitalbezug einschätzen" }).click();
+    for (let i = 0; i < 4; i++) {
+      await page.getByLabel("Ja").click();
+      await page.getByRole("button", { name: "Übernehmen" }).click();
+    }
+
+    // Click on each question link and verify it leads to the correct page
+    for (const question of questions) {
+      await page.getByRole("link", { name: question.title }).click();
+      await expect(page).toHaveURL(question.url);
+    }
+  });
+
+  test("first question is clickable on start", async ({ page }) => {
+    await page.goto(PATH_PRECHECK);
+    await page.getByRole("link", { name: "IT-System" }).click();
+    await expect(page).toHaveURL(questions[0].url);
+  });
+
+  test("only answered and first unanswered question are clickable", async ({
+    page,
+  }) => {
+    await page.goto(questions[0].url);
+    for (let i = 1; i < 5; i++) {
+      await expect(
+        page.getByRole("link", { name: questions[i].title }),
+      ).toBeDisabled();
+    }
+
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await expect(
+      page.getByRole("link", { name: questions[0].title }),
+    ).toBeEnabled();
+    for (let i = 2; i < 5; i++) {
+      await expect(
+        page.getByRole("link", { name: questions[i].title }),
+      ).toBeDisabled();
+    }
+
+    await page.goto(questions[0].url);
+    await expect(
+      page.getByRole("link", { name: questions[1].title }),
+    ).toBeEnabled();
+    for (let i = 2; i < 5; i++) {
+      await expect(
+        page.getByRole("link", { name: questions[i].title }),
+      ).toBeDisabled();
+    }
+  });
+
+  test("navigation resets when cookie is cleared", async ({ page }) => {
+    await page.goto(questions[0].url);
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+    await page.getByRole("link", { name: "Zurück" }).click();
+    await expect(
+      page.getByRole("link", { name: questions[0].title }),
+    ).toBeDisabled();
+  });
+
+  test("current page is disabled, has aria-current and active styling", async ({
+    page,
+  }) => {
+    await page.goto(questions[0].url);
+
+    // Check if the first question link is disabled and has aria-current attribute
+    await expect(
+      page.getByRole("link", { name: questions[0].title }),
+    ).toBeDisabled();
+    await expect(
+      page.getByRole("link", { name: questions[0].title }),
+    ).toHaveAttribute("aria-current", "true");
+    await expect(
+      page.getByRole("link", { name: questions[0].title }),
+    ).toHaveClass(/border-l-/);
+
+    // Navigate to the second question
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+
+    // Check if the second question link is disabled and has aria-current attribute
+    await expect(
+      page.getByRole("link", { name: questions[1].title }),
+    ).toBeDisabled();
+    await expect(
+      page.getByRole("link", { name: questions[1].title }),
+    ).toHaveAttribute("aria-current", "true");
+    await expect(
+      page.getByRole("link", { name: questions[1].title }),
+    ).toHaveClass(/border-l-/);
+  });
+
+  test("answered question have check mark icon", async ({ page }) => {
+    await page.goto(questions[0].url);
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+
+    // Check if the first question has a check mark icon
+    await expect(
+      page
+        .getByRole("link", { name: questions[0].title })
+        .getByTestId("CheckIcon"),
+    ).toBeVisible();
+
+    // Check second question is not checked
+    await expect(
+      page
+        .getByRole("link", { name: questions[1].title })
+        .getByTestId("CheckIcon"),
+    ).not.toBeVisible();
+
+    // Answer the second question
+    await page.getByLabel("Ja").click();
+    await page.getByRole("button", { name: "Übernehmen" }).click();
+
+    // Check if the second question has a check mark icon
+    await expect(
+      page
+        .getByRole("link", { name: questions[1].title })
+        .getByTestId("CheckIcon"),
+    ).toBeVisible();
+  });
+});
+
 test.describe("test result page", () => {
   test("happy path leads to positive result", async ({ page }) => {
     await page.goto(PATH_PRECHECK);
