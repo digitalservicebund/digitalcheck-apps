07e6b7ed1a0426c4245b7aba569ac487b66a9164
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-13 15:21:29 +0200

    Add modified TextArea A2J component

---
+++ packages/shared/components/Textarea.tsx
@@ -0,0 +1,57 @@
+import classNames from "classnames";
+import type { ReactNode } from "react";
+import InputError from "./InputError";
+import InputLabel from "./InputLabel";
+import RichText from "./RichText";
+
+type TextareaProps = Readonly<{
+  name: string;
+  description?: string;
+  label?: ReactNode;
+  placeholder?: string;
+  error?: string;
+  formId?: string;
+}>;
+
+const Textarea = ({
+  name,
+  description,
+  label,
+  error,
+  ...props
+}: TextareaProps) => {
+  const errorId = `${name}-error`;
+
+  return (
+    <div className="ds-stack-8">
+      {label && (
+        <InputLabel
+          classname={description ? "ds-heading-03-reg" : ""}
+          id={name}
+        >
+          {label}
+        </InputLabel>
+      )}
+      {description && (
+        <RichText className={"ds-body-01-reg"} markdown={description} />
+      )}
+      <textarea
+        id={name}
+        name={name}
+        className={classNames(
+          "ds-textarea forced-color-adjust-none placeholder-gray-600",
+          {
+            "has-error": error,
+          },
+        )}
+        aria-invalid={error !== undefined}
+        aria-describedby={error && errorId}
+        aria-errormessage={error && errorId}
+        {...props}
+      />
+      <InputError id={errorId}>{error}</InputError>
+    </div>
+  );
+};
+
+export default Textarea;
