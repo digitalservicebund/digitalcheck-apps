978d36d29fbbc452f97bc516059553dd49e8905c
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-14 15:08:12 +0200

    Fix shared public assets by copying in Vite

--- packages/dito/vite.config.ts
+++ packages/dito/vite.config.ts
@@ -1,11 +1,20 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import * as path from "node:path";
 import { defineConfig } from "vite";
 import { cjsInterop } from "vite-plugin-cjs-interop";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  // publicDir: "../shared/public",
   plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: `${path.resolve(__dirname, "../shared/public")}/*`,
+          dest: "./",
+        },
+      ],
+    }),
     remix(),
     tsconfigPaths(),
     cjsInterop({
