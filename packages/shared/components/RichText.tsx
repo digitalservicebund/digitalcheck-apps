90782180c8fc97b2d8ff8e1861004f4a6f7171c3
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-20 10:26:51 +0200

    Force PDF files linked in Markdown to download

--- packages/shared/components/RichText.tsx
+++ packages/shared/components/RichText.tsx
@@ -12,11 +12,16 @@ const RichText = ({ markdown, className, ...props }: RichTextProps) => {
   const renderer = {
     link(href: string, title: string | null | undefined, text: string) {
       const linkHtml = originalRenderer.link(href, title, text);
+      // Force external links to open in a new window
       if (href.startsWith("http")) {
         return linkHtml.replace(
           /^<a /,
           `<a target="_blank" aria-describedby=${A11Y_MESSAGE_NEW_WINDOW} `,
         );
+      }
+      // Force the browser to download links to PDF files
+      if (href.endsWith(".pdf")) {
+        return linkHtml.replace(/^<a /, `<a download `);
       }
       return linkHtml;
     },
