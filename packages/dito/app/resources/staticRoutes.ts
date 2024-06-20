e4b544ac540faa956a38a56129100c07384df4c3
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-20 16:17:50 +0200

    Add resource route to generate sitemap.xml

--- packages/dito/app/resources/staticRoutes.ts
+++ packages/dito/app/resources/staticRoutes.ts
@@ -2,10 +2,12 @@ export const PATH_LANDING: string = "/";
 export const PATH_PRECHECK: string = "/vorpruefung";
 export const PATH_RESULT: string = PATH_PRECHECK + "/ergebnis";
 export const PATH_ASSESSMENT: string = PATH_RESULT + "/einschaetzung";
-export const PATH_DOCUMENTATION: string = "/dokumentation";
 export const PATH_IMPRINT: string = "/impressum";
 export const PATH_PRIVACY: string = "/datenschutz";
 export const PATH_A11Y: string = "/barrierefreiheit";
+export const PATH_PRECHECK_PDF: string = "/assets/digitalcheck-vorpruefung.pdf";
+export const PATH_DOCUMENTATION_PDF: string =
+  "/assets/digitalcheck-begleitende-dokumentation.pdf";
 
 export type Route = {
   url: string;
@@ -32,6 +34,14 @@ const routes: Route[] = [
     url: PATH_ASSESSMENT,
     title: "Einschätzung als PDF",
     parent: PATH_RESULT,
+  },
+  {
+    url: PATH_PRECHECK_PDF,
+    title: "Digitalcheck Vorprüfung",
+  },
+  {
+    url: PATH_DOCUMENTATION_PDF,
+    title: "Digitalcheck Begleitendede Dokumentation",
   },
   // {
   //   url: PATH_IMPRINT,
