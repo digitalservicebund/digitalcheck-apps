9ea742efa49352656d28903cb3b9a045091ce222
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-21 12:56:20 +0200

    Add Feature Flags server component

---
+++ packages/dito/app/feature-flags.server.ts
@@ -0,0 +1,18 @@
+import { initialize } from "unleash-client";
+
+const unleash = initialize({
+  url: process.env.UNLEASH_API_URL || "",
+  appName: process.env.UNLEASH_APP || "",
+  customHeaders: {
+    Authorization: process.env.UNLEASH_KEY || "",
+  },
+});
+
+unleash.on("ready", console.log);
+unleash.on("synchronized", console.log);
+unleash.on("error", console.error);
+unleash.on("warn", console.warn);
+
+export { getFeatureToggleDefinitions } from "unleash-client";
+
+export default unleash;
