eeedd49e44e1d1df5cf480e098d8d47ae0b95778
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-24 13:28:07 +0200

    Add a test for PDF files in richtext

--- packages/shared/tests/unit/richtext.test.tsx
+++ packages/shared/tests/unit/richtext.test.tsx
@@ -8,7 +8,9 @@ const EXAMPLE_MARKDOWN = `Jedes Regelungsvorhaben hat Auswirkungen auf die Digit
 Schauen Sie sich die [Hilfestellungen und Methoden](/methoden) an, um Ihr Regelungsvorhaben digitaltauglich zu gestalten.
 Mit den folgenden externen Beispielen können Sie sich ein Bild davon machen, wie Digitaltauglichkeit in der Praxis aussieht:
 - [Beispiel 1](https://example.com)
-- [Beispiel 2](https://example.com)`;
+- [Beispiel 2](https://example.com)
+
+Hier können sie die [Dokumentation herunterladen](/assets/digitalcheck-dokumentation.pdf).`;
 
 test("Richtext outermost element has richtext class", () => {
   render(<RichText markdown={EXAMPLE_MARKDOWN} data-testid="rich-text" />);
@@ -21,4 +23,10 @@ test("Richtext renders external links with target blank", async () => {
   expect(links[0]).not.toHaveAttribute("target", "_blank");
   expect(links[1]).toHaveAttribute("target", "_blank");
   expect(links[2]).toHaveAttribute("target", "_blank");
+});
+
+test("Richtext links to PDF files have download attribute", async () => {
+  render(<RichText markdown={EXAMPLE_MARKDOWN} data-testid="rich-text" />);
+  const links = await screen.findAllByRole("link");
+  expect(links[3]).toHaveAttribute("download");
 });
