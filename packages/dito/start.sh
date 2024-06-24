aed03a328f3b5a117eeca6076ede33f620ec8c03
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-24 14:59:33 +0200

    Pipe env vars into docker image

---
+++ packages/dito/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# source '.env'
+
+set -euf
+
+# Exporting only if not already set, see https://stackoverflow.com/a/11686912
+export BASE_URL="${BASE_URL:=$(cat /etc/opaque/base-url)}"
+export UNLEASH_API_URL="${UNLEASH_API_URL:=$(cat /etc/opaque/unleash-api-url)}"
+export UNLEASH_APP="${UNLEASH_APP:=$(cat /etc/opaque/unleash-app)}"
+export UNLEASH_KEY="${UNLEASH_KEY:=$(cat /etc/sealed/unleash-key)}"
+
+npm run start --workspace packages/dito
