97082c03bedc41a3a7d7e808565b06a8a3f7f945
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-06-25 11:26:36 +0200

    Change location for production secrets

--- packages/dito/start.sh
+++ packages/dito/start.sh
@@ -3,10 +3,10 @@
 set -euf
 
 # Exporting only if not already set, see https://stackoverflow.com/a/11686912
-export BASE_URL="${BASE_URL:=$(cat /etc/opaque/base-url)}"
-export UNLEASH_API_URL="${UNLEASH_API_URL:=$(cat /etc/opaque/unleash-api-url)}"
-export UNLEASH_APP="${UNLEASH_APP:=$(cat /etc/opaque/unleash-app)}"
-export UNLEASH_KEY="${UNLEASH_KEY:=$(cat /etc/sealed/unleash-key)}"
+export BASE_URL="${BASE_URL:=$(cat /etc/secrets-opaque/base-url)}"
+export UNLEASH_API_URL="${UNLEASH_API_URL:=$(cat /etc/secrets-opaque/unleash-api-url)}"
+export UNLEASH_APP="${UNLEASH_APP:=$(cat /etc/secrets-opaque/unleash-app)}"
+export UNLEASH_KEY="${UNLEASH_KEY:=$(cat /etc/secrets-sealed/unleash-key)}"
 
 npm run start --workspace packages/dito
 
