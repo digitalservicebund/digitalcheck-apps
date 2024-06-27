e09e0dc2e1f7e3085a2fafcb3840c5abf5207797
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-27 17:02:14 +0200

    Upgrade to marked 13

--- packages/shared/components/RichText.tsx
+++ packages/shared/components/RichText.tsx
@@ -1,4 +1,4 @@
-import { Marked } from "marked";
+import { marked, Tokens } from "marked";
 import { A11Y_MESSAGE_NEW_WINDOW } from "./Aria";
 
 export type RichTextProps = {
@@ -7,27 +7,30 @@ export type RichTextProps = {
 };
 
 const RichText = ({ markdown, className, ...props }: RichTextProps) => {
-  const marked = new Marked();
-  const originalRenderer = new marked.Renderer();
-  const renderer = {
-    link(href: string, title: string | null | undefined, text: string) {
-      const linkHtml = originalRenderer.link(href, title, text);
-      // Force external links to open in a new window
-      if (href.startsWith("http")) {
-        return linkHtml.replace(
-          /^<a /,
-          `<a target="_blank" aria-describedby=${A11Y_MESSAGE_NEW_WINDOW} `,
-        );
-      }
-      // Force the browser to download links to PDF files
-      if (href.endsWith(".pdf")) {
-        return linkHtml.replace(/^<a /, `<a download `);
-      }
-      return linkHtml;
+  const renderer = new marked.Renderer();
+  const extension = {
+    useNewRenderer: true,
+    renderer: {
+      link(token: Tokens.Link) {
+        const { href } = token;
+        const linkHtml = renderer.link.call(this, token);
+        // Force external links to open in a new window
+        if (href.startsWith("http")) {
+          return linkHtml.replace(
+            /^<a /,
+            `<a target="_blank" aria-describedby=${A11Y_MESSAGE_NEW_WINDOW} `,
+          );
+        }
+        // Force the browser to download links to PDF files
+        if (href.endsWith(".pdf")) {
+          return linkHtml.replace(/^<a /, `<a download `);
+        }
+        return linkHtml;
+      },
     },
   };
 
-  marked.use({ renderer });
+  marked.use(extension);
   const html = marked.parse(markdown);
 
   return html ? (
