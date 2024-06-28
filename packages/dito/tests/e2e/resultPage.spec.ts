51d7ca77eec89556725764c7841fbbf349b0f910
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-28 13:00:29 +0200

    Fix some more page.waitForURL issues

--- packages/dito/tests/e2e/resultPage.spec.ts
+++ packages/dito/tests/e2e/resultPage.spec.ts
@@ -86,7 +86,7 @@ test.describe("test result page reasoning", () => {
     await page.getByLabel("Ich bin unsicher").click();
     await page.getByRole("button", { name: "Übernehmen" }).click();
     for (let i = 0; i < 4; i++) {
-      await page.waitForURL(preCheck.questions[i].url);
+      await page.waitForURL(preCheck.questions[i + 1].url);
       await page.getByLabel("Nein").click();
       await page.getByRole("button", { name: "Übernehmen" }).click();
     }
