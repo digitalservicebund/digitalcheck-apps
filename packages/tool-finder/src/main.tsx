3f311c040fc7a4e6184682dd780805c0036cca3e
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-06-28 14:07:44 +0200

    Fix styles import

--- packages/tool-finder/src/main.tsx
+++ packages/tool-finder/src/main.tsx
@@ -2,9 +2,9 @@ import "@digitalcheck/shared/styles.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import "styles.css";
 import App from "./App.tsx";
 import MaintenanceModeApp from "./MaintenanceModeApp";
+import "./styles.css";
 
 const maintenanceMode: boolean =
   import.meta.env.VITE_MAINTENANCE_MODE == "true";
