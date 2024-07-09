9edfae3cf7c50b74a7b510bceaa9a80dadfb4cd8
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-09 09:17:30 +0200

    Add feature flag for method subpages

--- packages/dito/app/routes/methoden.tsx
+++ packages/dito/app/routes/methoden.tsx
@@ -2,15 +2,34 @@ import Background from "@digitalcheck/shared/components/Background";
 import Container from "@digitalcheck/shared/components/Container";
 import Header from "@digitalcheck/shared/components/Header";
 import List from "@digitalcheck/shared/components/List";
-import { MetaFunction } from "@remix-run/react";
+import { json } from "@remix-run/node";
+import { MetaFunction, useLoaderData } from "@remix-run/react";
 import FeedbackBanner from "components/FeedbackBanner";
 import { methods, siteMeta } from "resources/content";
+import unleash from "utils/feature-flags.server";
 
 export const meta: MetaFunction = () => {
   return [{ title: `${methods.title} — ${siteMeta.title}` }];
 };
 
+export const loader = () => {
+  const methodSubpagesEnabled = unleash.isEnabled(
+    "digitalcheck.method-subpages",
+  );
+
+  return json({ methodSubpagesEnabled });
+};
+
 export default function Index() {
+  // FEATURE FLAG: Disable subpages in production, as they aren't complete yet
+  const { methodSubpagesEnabled } = useLoaderData<typeof loader>();
+  const methodStepsItems = methodSubpagesEnabled
+    ? methods.steps.items
+    : methods.steps.items.map((item) => {
+        item.buttons = [];
+        return item;
+      });
+
   return (
     <>
       <Background backgroundColor="blue">
@@ -27,7 +46,9 @@ export default function Index() {
         </Container>
       </Background>
       <Container>
-        <List items={methods.steps.items} />
+        <List items={methodStepsItems} />
+        {/* FEATURE FLAG: Revert to this once feature flag is no longer needed */}
+        {/* <List items={methods.steps.items} /> */}
       </Container>
       <Container>
         <List
