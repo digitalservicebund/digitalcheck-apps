5e4055de93ae201e3cb2a02819abc0d594f50833
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-09 11:27:49 +0200

    Inject icons on method pages

--- packages/dito/app/routes/methoden.tsx
+++ packages/dito/app/routes/methoden.tsx
@@ -2,9 +2,13 @@ import Background from "@digitalcheck/shared/components/Background";
 import Container from "@digitalcheck/shared/components/Container";
 import Header from "@digitalcheck/shared/components/Header";
 import List from "@digitalcheck/shared/components/List";
+import ContactSupportOutlined from "@digitalservicebund/icons/ContactSupportOutlined";
+import GroupOutlined from "@digitalservicebund/icons/GroupOutlined";
+import TimerOutlined from "@digitalservicebund/icons/TimerOutlined";
 import { json } from "@remix-run/node";
 import { MetaFunction, useLoaderData } from "@remix-run/react";
 import FeedbackBanner from "components/FeedbackBanner";
+import { renderToString } from "react-dom/server";
 import { methods, siteMeta } from "resources/content";
 import unleash from "utils/feature-flags.server";
 
@@ -22,6 +26,34 @@ export const loader = () => {
 
 export default function Index() {
   const { methodSubpagesEnabled } = useLoaderData<typeof loader>();
+
+  // This messy code is a hacky solution to inject icons into the content, while preserving the ability to modify content easily via Markdown
+  const iconClassName = `
+    inline-flex flex-row items-baseline align-baseline gap-4
+    [&_svg]:h-16
+    [&_svg]:w-16
+    [&_svg]:relative
+    [&_svg]:-bottom-2
+  `;
+  const timerOutlined = renderToString(
+    <strong className={iconClassName}>
+      <TimerOutlined />
+      Zeit:
+    </strong>,
+  );
+  const groupOutlined = renderToString(
+    <strong className={iconClassName}>
+      <GroupOutlined />
+      Kollaborativ:
+    </strong>,
+  );
+  const contactSupportOutlined = renderToString(
+    <strong className={iconClassName}>
+      <ContactSupportOutlined />
+      Support:
+    </strong>,
+  );
+
   const methodStepsItems = methods.steps.items.map((item) => {
     // FEATURE FLAG: Disable subpages in production, as they aren't complete yet
     if (!methodSubpagesEnabled) {
@@ -29,17 +61,11 @@ export default function Index() {
     }
 
     // Modify HTML to be able to style icons
-    item.content = item.content.replaceAll(
-      "**Zeit:**",
-      `<strong data-icon="TimerOutlined">Zeit:</strong>`,
-    );
-    item.content = item.content.replaceAll(
-      "**Kollaborativ:**",
-      `<strong data-icon="GroupOutlined">Kollaborativ:</strong>`,
-    );
+    item.content = item.content.replaceAll("**Zeit:**", timerOutlined);
+    item.content = item.content.replaceAll("**Kollaborativ:**", groupOutlined);
     item.content = item.content.replaceAll(
       "**Support:**",
-      `<strong data-icon="ContactSupportOutlined">Support:</strong>`,
+      contactSupportOutlined,
     );
 
     return item;
