ca53e4201952c26f333e6393b489fe912ead5460
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-10 17:20:52 +0200

    Configure global smooth scrolling

--- packages/dito/app/root.tsx
+++ packages/dito/app/root.tsx
@@ -110,7 +110,7 @@ function Document({
 }>) {
   const nonce = useNonce();
   return (
-    <html lang="de">
+    <html lang="de" className="scroll-smooth">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
