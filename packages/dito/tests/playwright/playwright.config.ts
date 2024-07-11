2975fb9e122e50e92389a2a20a2b30bcc590ef84
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-07-11 15:24:28 +0200

    Move playwright tests and config to own folder

--- packages/dito/tests/playwright/playwright.config.ts
+++ packages/dito/tests/playwright/playwright.config.ts
@@ -1,11 +1,11 @@
 import { PlaywrightTestConfig } from "@playwright/test";
-import baseConfig from "../../../playwright.config-base";
+import baseConfig from "../../../../playwright.config-base";
 
 const config: PlaywrightTestConfig = {
   ...baseConfig,
   webServer: {
-    command: "npm run build && npm start",
-    port: 4173,
+    command: "npm run dev",
+    port: 5173,
     timeout: parseInt(process.env.WAIT_ON_TIMEOUT ?? `${5 * 1000}`),
     reuseExistingServer: true,
   },
