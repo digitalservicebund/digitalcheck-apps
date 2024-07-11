ca8547a71ac72f42cc9254e3d284319d8787b997
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-11 11:38:46 +0200

    Content for responsible actors

--- packages/dito/app/resources/content.ts
+++ packages/dito/app/resources/content.ts
@@ -535,6 +535,54 @@ export const responsibleActors = {
   subtitle: `Wenn Sie ein Regelungsvorhaben erstellen, ist es entscheidend, die Zuständigkeiten der umsetzenden Akteurinnen und Akteure zu kennen: Sie sind die Expertinnen und Experten für die Umsetzung in der Praxis und damit die wertvollsten Gesprächspartnerinnen und -partner für Digitaltauglichkeit.
   
   Zeitaufwand: ca. vier Stunden`,
+  accordion: {
+    title:
+      "Was ist zu tun, wenn Länder oder Kommunen für die Umsetzung zuständig sind?",
+    text: "I don't know!",
+  },
+  content: {
+    label: "Anleitung",
+    title: "Finden Sie konstruktive Gesprächspartnerinnen und -partner",
+    text: `Beginnen Sie mit der Kontaktaufnahme oben in der Hierarchie, lassen Sie ggf. über Ihre Referats- und (Unter-)abteilungsleitung den Kontakt herstellen. Fragen Sie nach den Fachexpertinnen und -experten auf Arbeitsebene, hier steckt in der Regel das tiefste Praxiswissen.
+
+- **Kommunen:** Bitten Sie Ansprechpersonen auf Landesebene um Kontakte und nutzen Sie  das gesammelte Wissen in den Kommunalen Spitzenverbänden. 
+- **Behördenund Träger:** Nutzen Sie die offiziellen Wege der Häuser.
+- **Unternehmen, Sozialpartner, weitere Organisationen:** Fragen Sie in Spitzenverbänden nach Ansprechpersonen für Ihren konkreten Anwendungsfall. 
+
+Wenn Sie keine persönlichen Kontakte nutzen können, greifen sie auf Organigramme oder interne Datenbanken zu, z. B. das X500-Verzeichnis.`,
+  },
+  boxes: [
+    {
+      image: {
+        src: "/assets/images/ebenen-auswaehlen-und-ansprechpersonen-sammeln.png", // TODO: Image doesn't exist yet
+        alt: `Eine Excel-Tabelle mit dem Titel "Beteiligte Ebenen auswählen und Ansprechpersonen sammeln für [ARBEITSTITEL]" enthält Spalten für Name, Zuständigkeit, Akteursgruppe, Kontaktdaten und Bemerkungen. Der erste Eintrag listet als Beispiel "Maria Muster" als Referentin für das Statistische Bundesamt, zugehörig zur Akteursgruppe "Bund" mit ihren Kontaktdaten.`,
+      },
+      label: "Vorlage",
+      title: "Beteiligte Ebenen auswählen und Ansprechpersonen sammeln",
+      text: `Die Excelvorlage hilft Ihnen, die beteiligten Ebenen auszuwählen, Zuständigkeiten zu klären und hilfreiche Ansprechpersonen zu sammeln.`,
+      buttons: [
+        {
+          text: "Vorlage herunterladen",
+          href: "/assets/ansprechpersonen-sammeln.xlsx", // TODO: File doesn't exist yet...
+        },
+      ],
+    },
+  ],
+  tip: {
+    label: "Tipps",
+    title: "Gespräche über die aktuelle Umsetzungspraxis",
+    text: `- **Keine Sorge vor falscher Kontaktaufnahme:** Es kann nichts passieren, außer dass man Sie an die richtige Ansprechperson verweist.
+    - **Persönliche Gespräche statt Schriftverkehr:** Bitten Sie um persönliche Gespräche bei der Kontaktaufnahme. Schriftlicher Austausch lädt zu Missverständnissen ein.  
+    - **Der Austausch über den Ist-Stand ist sicher:** Nur Mut bei der Ansprache, über die aktuelle Umsetzung dürfen Sie immer sprechen.`,
+  },
+  nextStep: {
+    label: "So geht es weiter:",
+    title: "Aufgaben und Abläufe gemeinsam erfassen",
+    text: `Damit Ihre Regelung wirkungsvoll in die Praxis kommt, müssen Sie die **aktuellen Rahmenbedingungen** verstehen. Holen Sie sich dazu Unterstützung von den Akteurinnen und Akteuren, die Sie identifiziert haben. Sie müssen noch nicht auf geplante Neuerungen eingehen. Konzentrieren Sie sich auf den **Status Quo**.`,
+    buttons: [
+      { text: "Zum nächsten Schritt", href: PATH_METHODS_TASKS_PROCESSES },
+    ],
+  },
 };
 
 export const tasksProcesses = {
