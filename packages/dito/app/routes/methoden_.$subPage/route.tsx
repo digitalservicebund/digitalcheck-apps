6674f3086ab7310819ce08b21201c6023a83cb58
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-15 14:00:20 +0200

    Template for method subpage

--- packages/dito/app/routes/methoden_.$subPage/route.tsx
+++ packages/dito/app/routes/methoden_.$subPage/route.tsx
@@ -1,6 +1,12 @@
 import Background from "@digitalcheck/shared/components/Background";
+import Button from "@digitalcheck/shared/components/Button";
+import ButtonContainer from "@digitalcheck/shared/components/ButtonContainer";
 import Container from "@digitalcheck/shared/components/Container";
+import DetailsSummary from "@digitalcheck/shared/components/DetailsSummary";
 import Header from "@digitalcheck/shared/components/Header";
+import Heading from "@digitalcheck/shared/components/Heading";
+import Image from "@digitalcheck/shared/components/Image";
+import RichText from "@digitalcheck/shared/components/RichText";
 import { LoaderFunctionArgs } from "@remix-run/node";
 import { json, MetaFunction, useLoaderData } from "@remix-run/react";
 import FeedbackBanner from "components/FeedbackBanner";
@@ -19,10 +25,6 @@ import {
   PATH_METHODS_TECHNICAL_FEASIBILITY,
 } from "resources/staticRoutes";
 
-export const meta: MetaFunction = () => {
-  return [{ title: `${tasksProcesses.title} — ${siteMeta.title}` }];
-};
-
 export type TMethodPage = {
   title: string;
   subtitle: string;
@@ -30,7 +32,7 @@ export type TMethodPage = {
     title: string;
     text: string;
   };
-  content?: {
+  content: {
     label: string;
     title: string;
     text: string;
@@ -45,9 +47,13 @@ export type TMethodPage = {
     text: string;
     buttons?: { text: string; href: string }[];
   }[];
+  tip?: {
+    label: string;
+    title: string;
+    text: string;
+  };
   support?: {
     label: string;
-
     title: string;
     text: string;
   };
@@ -94,6 +100,12 @@ export function loader({ params }: LoaderFunctionArgs) {
   return json(content);
 }
 
+export const meta: MetaFunction<typeof loader> = ({ data }) => {
+  if (data) {
+    return [{ title: `${data.title} — ${siteMeta.title}` }];
+  }
+};
+
 export default function Index() {
   const content = useLoaderData<typeof loader>();
 
@@ -112,6 +124,113 @@ export default function Index() {
           ></Header>
         </Container>
       </Background>
+      {content.accordion && (
+        <Container>
+          <DetailsSummary
+            title={content.accordion.title}
+            content={content.accordion.text}
+          />
+        </Container>
+      )}
+      <Container>
+        <div className="flex flex-col gap-16">
+          <Heading
+            tagName="div"
+            className="ds-label-section text-gray-900"
+            text={content.content.label}
+          />
+          <Heading tagName="h2" text={content.content.title} />
+          <RichText markdown={content.content.text} />
+        </div>
+      </Container>
+      {content.boxes?.map((box) => (
+        <Container key={box.title}>
+          {box.image && (
+            <div className="rounded-t-lg overflow-hidden">
+              <Background backgroundColor="midBlue">
+                <div className="p-64 pb-0">
+                  <div className="rounded-t-lg shadow-2xl overflow-hidden h-0 pb-[40%] &_img:object-cover &_img:object-top">
+                    <Image
+                      url={box.image.src}
+                      alternativeText={box.image.alt}
+                    />
+                  </div>
+                </div>
+              </Background>
+            </div>
+          )}
+          <div className="rounded-b-lg overflow-hidden">
+            <Background backgroundColor="blue">
+              <div className="flex flex-col gap-16 p-64">
+                <Heading
+                  tagName="div"
+                  className="ds-label-section text-gray-900"
+                  text={box.label}
+                />
+                <Heading tagName="h2" text={box.title} />
+                <RichText markdown={box.text} />
+                {box.buttons && (
+                  <ButtonContainer>
+                    {box.buttons.map((button) => (
+                      <Button key={button.text ?? button.href} {...button} />
+                    ))}
+                  </ButtonContainer>
+                )}
+              </div>
+            </Background>
+          </div>
+        </Container>
+      ))}
+      {content.tip && (
+        <Background backgroundColor="yellow">
+          <Container>
+            <div className="flex flex-col gap-16">
+              <Heading
+                tagName="div"
+                className="ds-label-section text-gray-900"
+                text={content.tip.label}
+              />
+              <Heading tagName="h2" text={content.tip.title} />
+              <RichText markdown={content.tip.text} />
+            </div>
+          </Container>
+        </Background>
+      )}
+      {content.support && (
+        <Background backgroundColor="blue">
+          <Container>
+            <div className="flex flex-col gap-16">
+              <Heading
+                tagName="div"
+                className="ds-label-section text-gray-900"
+                text={content.support.label}
+              />
+              <Heading tagName="h2" text={content.support.title} />
+              <RichText markdown={content.support.text} />
+            </div>
+          </Container>
+        </Background>
+      )}
+      {content.nextStep && (
+        <Container>
+          <div className="flex flex-col gap-16">
+            <Heading
+              tagName="div"
+              className="ds-label-section text-gray-900"
+              text={content.nextStep.label}
+            />
+            <Heading tagName="h2" text={content.nextStep.title} />
+            <RichText markdown={content.nextStep.text} />
+            {content.nextStep.buttons && (
+              <ButtonContainer>
+                {content.nextStep.buttons.map((button) => (
+                  <Button key={button.text ?? button.href} {...button} />
+                ))}
+              </ButtonContainer>
+            )}
+          </div>
+        </Container>
+      )}
       <FeedbackBanner />
     </>
   );
