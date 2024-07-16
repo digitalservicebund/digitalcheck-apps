6b161463be9b24dfc1b5b72e04d48f7cbd6d9f17
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-07-16 12:02:55 +0200

    Fix styling of Details

--- packages/shared/components/DetailsSummary.tsx
+++ packages/shared/components/DetailsSummary.tsx
@@ -11,11 +11,9 @@ export default function DetailsSummary({
   content,
 }: DetailsSummaryProps) {
   return (
-    <details className="details focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800 ds-label-01-bold">
-      <summary className="summary-content inline-flex focus:outline-none cursor-pointer list-none bg-no-repeat pl-[24px]">
-        {title}
-      </summary>
-      <span className="block ds-label-01-reg pt-4  pl-[24px] text-black">
+    <details className="focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800 ds-label-01-bold">
+      <summary className=" focus:outline-none cursor-pointer">{title}</summary>
+      <span className="block ds-label-01-reg pt-4 pl-16 text-black">
         {content && <RichText markdown={content} />}
       </span>
     </details>
