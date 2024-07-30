5a9ec399cd775c6807e8321c34e7fbd39227ab64
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-07-30 17:11:39 +0200

    Update InterviewBanner.tsx

--- packages/dito/app/components/InterviewBanner.tsx
+++ packages/dito/app/components/InterviewBanner.tsx
@@ -1,24 +1,23 @@
-import Background from "@digitalcheck/shared/components/Background";
 import Box from "@digitalcheck/shared/components/Box";
 import Container from "@digitalcheck/shared/components/Container";
 import { interviewBanner } from "resources/content";
 
 export default function InterviewBanner() {
   return (
-    <Container additionalClassNames="!p-0">
-      <Background backgroundColor="blue">
-        <Container>
-          <Box
-            heading={{
-              tagName: "h2",
-              text: interviewBanner.title,
-            }}
-            content={{
-              markdown: interviewBanner.text,
-            }}
-          ></Box>
-        </Container>
-      </Background>
+    <Container
+      backgroundColor="blue"
+      overhangingBackground
+      additionalClassNames="mt-40 mb-48"
+    >
+      <Box
+        heading={{
+          tagName: "h2",
+          text: interviewBanner.title,
+        }}
+        content={{
+          markdown: interviewBanner.text,
+        }}
+      ></Box>
     </Container>
   );
 }
