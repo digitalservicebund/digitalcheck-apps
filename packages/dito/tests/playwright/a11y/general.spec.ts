03f8ca5ca8b0552089640c6c2c99c5e7f6cdf3a5
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-07-30 10:19:31 +0200

    Test a11y for all routes individually

--- packages/dito/tests/playwright/a11y/general.spec.ts
+++ packages/dito/tests/playwright/a11y/general.spec.ts
@@ -4,14 +4,13 @@ import { expect, test } from "@playwright/test";
 import allRoutes from "resources/allRoutes";
 
 test.describe("basic example a11y test", () => {
-  test("check a11y of all routes", async ({ page }) => {
-    for await (const route of allRoutes.filter(
-      (allRoute) => !allRoute.url.endsWith(".pdf"),
-    )) {
+  for (const route of allRoutes.filter(
+    (allRoute) => !allRoute.url.endsWith(".pdf"),
+  )) {
+    test(`check a11y of all route: ${route.url}`, async ({ page }) => {
       await page.goto(route.url);
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      console.log("Checking A11Y on route:", route.url);
       expect(accessibilityScanResults.violations).toEqual([]);
-    }
-  });
+    });
+  }
 });
