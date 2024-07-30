819489fef5ddf89054a70321972926a237067f61
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-07-30 14:29:44 +0200

    Reapply "Change postcss config"
    This reverts commit 963f355a1f585cda02e3a9af20888940c7d98ebe.

--- packages/tool-finder/postcss.config.js
+++ packages/tool-finder/postcss.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
     tailwindcss: { config: "./tailwind.config.js" },
     autoprefixer: {},
   },
 };
+
+export default config;
