5e5ff992e884e761353dd552cb077c5be21fc9c4
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-08-07 14:45:21 +0200

    Allow iframes from google calendar

--- packages/dito/app/entry.server.tsx
+++ packages/dito/app/entry.server.tsx
@@ -117,7 +117,7 @@ function handleBrowserRequest(
 
   responseHeaders.set(
     "Content-Security-Policy",
-    `default-src 'none'; script-src 'self' https: 'nonce-${nonce}'; style-src 'self'; font-src 'self'; img-src 'self'; frame-ancestors 'self'; connect-src 'self' https://plausible.io`,
+    `default-src 'none'; script-src 'self' https: 'nonce-${nonce}'; style-src 'self'; font-src 'self'; img-src 'self'; frame-ancestors 'self' https://calendar.google.com https://calendar.app.google; frame-src 'self' https://calendar.google.com; connect-src 'self' https://plausible.io`,
   );
 
   return new Promise((resolve, reject) => {
