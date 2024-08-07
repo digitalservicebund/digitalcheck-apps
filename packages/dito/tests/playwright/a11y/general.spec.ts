f76750a76f7f83618e84c8831486f2aaec1c2cd9
Mike Pierce <mike.pierce@digitalservice.bund.de>
2024-08-07 15:47:41 +0200

    Skip a11y test for support page

--- packages/dito/tests/playwright/a11y/general.spec.ts
+++ packages/dito/tests/playwright/a11y/general.spec.ts
@@ -2,11 +2,14 @@ import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 import allRoutes from "resources/allRoutes";
+import { ROUTE_SUPPORT } from "resources/staticRoutes";
 
 test.describe("basic example a11y test", () => {
   test("check a11y of all routes", async ({ page }) => {
     for await (const route of allRoutes.filter(
-      (allRoute) => !allRoute.url.endsWith(".pdf"),
+      (allRoute) =>
+        !allRoute.url.endsWith(".pdf") &&
+        !allRoute.url.endsWith(ROUTE_SUPPORT.url),
     )) {
       await page.goto(route.url);
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
