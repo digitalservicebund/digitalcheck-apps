e267798beabac3d55fc1cf93aaa7d0c6c3c2a3f2
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-09-13 16:02:43 +0200

    Add support page tests

---
+++ packages/dito/tests/playwright/e2e/support.spec.ts
@@ -0,0 +1,42 @@
+import { expect, test } from "@playwright/test";
+import * as staticRoutes from "resources/staticRoutes";
+
+test.describe("test general availability", () => {
+  test.beforeEach("Go to support page", async ({ page }) => {
+    await page.goto(staticRoutes.ROUTE_SUPPORT.url);
+  });
+
+  test("clicking on appointment button shows google and hides button", async ({
+    page,
+  }) => {
+    await page.getByRole("button", { name: "Termin buchen" }).click();
+    await expect(
+      page.getByRole("button", { name: "Termin buchen" }),
+    ).toBeHidden();
+    await expect(
+      page
+        .frameLocator(
+          'iframe[title="Beratung erhalten Sie in einem 45-minütigem Gespräch"]',
+        )
+        .getByRole("heading", { name: "Unterstützungsangebot" }),
+    ).toBeVisible();
+  });
+
+  test("suppport tabs switch between offerings", async ({ page }) => {
+    await page.getByRole("tab", { name: "Schnelle Hilfe" }).click();
+    await expect(
+      page.getByRole("heading", { name: "IT-Wissen" }),
+    ).toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Digitale Umsetzung erarbeiten" }),
+    ).not.toBeVisible();
+
+    await page.getByRole("tab", { name: "Umfangreiche Beratung" }).click();
+    await expect(
+      page.getByRole("heading", { name: "IT-Wissen" }),
+    ).not.toBeVisible();
+    await expect(
+      page.getByRole("heading", { name: "Digitale Umsetzung erarbeiten" }),
+    ).toBeVisible();
+  });
+});
