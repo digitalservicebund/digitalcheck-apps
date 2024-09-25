c6baa46bde42a1262e15145771848248918c92d1
lucas <lucas.solcher@digitalservice.bund.de>
2024-09-25 10:54:52 +0200

    Remove scroll behaviour

--- packages/dito/app/root.tsx
+++ packages/dito/app/root.tsx
@@ -279,7 +279,7 @@ function Document({
   const nonce = useNonce();
 
   return (
-    <html lang="de" className="scroll-smooth">
+    <html lang="de">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
