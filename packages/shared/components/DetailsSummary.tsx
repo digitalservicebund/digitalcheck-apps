3a078b114d9d70210adc92172fcb5c03e7d380b1
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-11-13 16:14:57 +0100

    Add open prop to DetailsSummary

--- packages/shared/components/DetailsSummary.tsx
+++ packages/shared/components/DetailsSummary.tsx
@@ -5,14 +5,19 @@ export type DetailsSummaryProps = {
   identifier?: string;
   title?: string;
   content?: string | ReactNode;
+  open?: boolean;
 };
 
 export default function DetailsSummary({
   title,
   content,
+  open = false,
 }: DetailsSummaryProps) {
   return (
-    <details className="focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800 ds-label-01-bold">
+    <details
+      open={open}
+      className="focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800 ds-label-01-bold"
+    >
       <summary className=" focus:outline-none cursor-pointer">{title}</summary>
       <span className="block ds-label-01-reg pt-4 pl-16 text-black">
         {typeof content === "string" ? (
