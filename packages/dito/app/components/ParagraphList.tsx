6c77b15d8327034793ad3cfe5f2ea11f188007de
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-11-15 01:51:50 +0100

    Improve Paragraph component
    - Group Absaetze without Erfuellungen together and hide in DetailView
    - Show Absaetze with Erfuellungen without DetailView
    - Move WarumGut to each Absatz and always show
    - Use different colors for principle highlighting
    - Change border looks

--- packages/dito/app/components/ParagraphList.tsx
+++ packages/dito/app/components/ParagraphList.tsx
@@ -1,8 +1,111 @@
 import DetailsSummary from "@digitalcheck/shared/components/DetailsSummary.tsx";
 import Heading from "@digitalcheck/shared/components/Heading.tsx";
 import { BlocksRenderer } from "@strapi/blocks-react-renderer";
-import type { Paragraph, Prinzip } from "../utils/strapiData.server.ts";
-import AbsatzRenderer from "./AbsatzRenderer.tsx";
+import { ReactNode } from "react";
+import type {
+  Absatz,
+  Paragraph,
+  Prinzip,
+  PrinzipErfuellung,
+} from "../utils/strapiData.server.ts";
+
+// Color mapping for different principles
+const HIGHLIGHT_COLORS = {
+  1: { background: "bg-yellow-300", border: "border-yellow-500" },
+  2: { background: "bg-blue-300", border: "border-blue-500" },
+  3: { background: "bg-red-300", border: "border-red-500" },
+  4: { background: "bg-purple-300", border: "border-purple-500" },
+  5: { background: "bg-green-300", border: "border-green-500" },
+} as const;
+
+type AbsatzGroup = {
+  start: number;
+  end?: number;
+  absaetze: Absatz[];
+};
+
+function PrincipleHighlight({ children }: { children: ReactNode }) {
+  if (!children || typeof children !== "object" || !("props" in children)) {
+    return null;
+  }
+
+  const parts = (children.props.children as string).split(/(\[\d\])/g);
+  const number = Number(parts[1][1]) as keyof typeof HIGHLIGHT_COLORS;
+
+  return (
+    <mark className={HIGHLIGHT_COLORS[number].background}>
+      {parts[0]}
+      <sup>{`P${number}`}</sup>
+    </mark>
+  );
+}
+
+const PrincipleExplanation = ({
+  erfuellung,
+}: {
+  erfuellung: PrinzipErfuellung;
+}) => (
+  <div
+    className={`border-l-4 ${HIGHLIGHT_COLORS[erfuellung.Prinzip.Nummer].border} pl-4`}
+  >
+    <Heading
+      tagName="h4"
+      text={`P${erfuellung.Prinzip.Nummer} – ${erfuellung.Prinzip.Name}`}
+      look="ds-label-01-bold"
+    />
+    <BlocksRenderer content={erfuellung.WarumGut} />
+  </div>
+);
+
+const AbsatzContent = ({
+  absatzGroup,
+}: {
+  absatzGroup: Absatz | AbsatzGroup;
+}) => {
+  if ("id" in absatzGroup) {
+    return (
+      <div>
+        <BlocksRenderer
+          content={absatzGroup.Text}
+          modifiers={{
+            underline: PrincipleHighlight,
+          }}
+        />
+        {absatzGroup.PrinzipErfuellungen.length > 0 && (
+          <div className="ds-stack-8 mt-8">
+            {/* TODO: move to content file */}
+            <span className="ds-label-01-bold italic">Warum ist das gut?</span>
+            {absatzGroup.PrinzipErfuellungen.sort(
+              (a, b) => a.Prinzip.Nummer - b.Prinzip.Nummer,
+            ).map((erfuellung) => (
+              <PrincipleExplanation
+                key={erfuellung.id}
+                erfuellung={erfuellung}
+              />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+
+  const title = absatzGroup.end
+    ? `(${absatzGroup.start + 1}) – (${absatzGroup.end})`
+    : `(${absatzGroup.start + 1})`;
+
+  return (
+    <DetailsSummary
+      title={title}
+      content={
+        <div className="ds-stack-8">
+          {absatzGroup.absaetze.map((absatz) => (
+            <BlocksRenderer key={absatz.id} content={absatz.Text} />
+          ))}
+        </div>
+      }
+    />
+  );
+};
 
 function Paragraph({
   paragraph,
@@ -11,67 +114,59 @@ function Paragraph({
   paragraph: Paragraph;
   principlesToFilter: Prinzip[];
 }) {
+  // Filter relevant principles
   const principleNumbers = principlesToFilter.map(
     (principle) => principle.Nummer,
   );
-
-  // only show PrinzipErfuellungen for the currently filtered Prinzipien
   const filteredAbsaetze = paragraph.Absaetze.map((absatz) => ({
     ...absatz,
     PrinzipErfuellungen: absatz.PrinzipErfuellungen.filter((erfuellung) =>
       principleNumbers.includes(erfuellung.Prinzip.Nummer),
     ),
   }));
-  const prinzipErfuellungen = paragraph.Absaetze.flatMap(
-    (absatz) => absatz.PrinzipErfuellungen,
+
+  // Group consecutive Absaetze without a relevant PrinzipErfuellungen together
+  const groupedAbsaetze = filteredAbsaetze.reduce(
+    (groups, absatz, index) => {
+      // If the Absatz has Erfuellungen, add it as a standalone item
+      if (absatz.PrinzipErfuellungen.length) {
+        groups.push(absatz);
+        return groups;
+      }
+      const lastGroup = groups[groups.length - 1];
+      // Start a new group if:
+      // 1. There are no previous groups, or
+      // 2. The last item is a single paragraph (has an 'id')
+      if (!lastGroup || "id" in lastGroup) {
+        groups.push({
+          start: index,
+          absaetze: [absatz],
+        });
+        return groups;
+      }
+      // Add to existing group
+      lastGroup.end = index;
+      lastGroup.absaetze.push(absatz);
+      return groups;
+    },
+    [] as (Absatz | AbsatzGroup)[],
   );
 
   return (
     <div key={paragraph.Nummer}>
       <div className="ds-stack-8 rich-text">
         <p className="ds-label-01-bold">
-          {paragraph.Nummer} {paragraph.Gesetz}
+          § {paragraph.Nummer} {paragraph.Gesetz}
         </p>
         <p className="ds-label-01-bold">{paragraph.Titel}</p>
-        {filteredAbsaetze.map((absatz, index) => (
-          <div key={absatz.id} className="border-l-4 border-gray-300 pl-8">
-            <DetailsSummary
-              title={`Absatz ${index + 1}`}
-              open={absatz.PrinzipErfuellungen.length > 0}
-              content={
-                <AbsatzRenderer
-                  text={absatz.Text}
-                  principlesToFilter={principleNumbers}
-                />
-              }
+        <div className="border-l-4 border-gray-300 pl-8 ds-stack-16">
+          {groupedAbsaetze.map((absatzGroup) => (
+            <AbsatzContent
+              key={"id" in absatzGroup ? absatzGroup.id : absatzGroup.start}
+              absatzGroup={absatzGroup}
             />
-          </div>
-        ))}
-        <DetailsSummary
-          title="Warum ist das gut?"
-          content={principlesToFilter.map((principle) => (
-            <div key={principle.Nummer}>
-              {principlesToFilter.length > 1 && (
-                <Heading
-                  tagName="h4"
-                  text={principle.Name}
-                  look="ds-label-01-bold"
-                />
-              )}
-              {prinzipErfuellungen.map(
-                (erfuellung) =>
-                  erfuellung.Prinzip.Nummer === principle.Nummer && (
-                    <div key={erfuellung.id}>
-                      <BlocksRenderer
-                        key={erfuellung.id}
-                        content={erfuellung.WarumGut}
-                      />
-                    </div>
-                  ),
-              )}
-            </div>
           ))}
-        />
+        </div>
       </div>
     </div>
   );
