29fa6ce16bf4aa7c537e3a4560c375db806aaafa
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-11-22 15:42:51 +0100

    Make DetailsSummary fontWeight controllable

--- packages/shared/components/DetailsSummary.tsx
+++ packages/shared/components/DetailsSummary.tsx
@@ -1,3 +1,4 @@
+import classNames from "classnames";
 import { ReactNode } from "react";
 import RichText from "./RichText";
 
@@ -5,20 +6,26 @@ export type DetailsSummaryProps = {
   identifier?: string;
   title?: string;
   content?: string | ReactNode;
+  bold?: boolean;
   open?: boolean;
 };
 
 export default function DetailsSummary({
   title,
   content,
+  bold = true,
   open = false,
 }: DetailsSummaryProps) {
+  const summaryClasses = classNames("focus:outline-none cursor-pointer", {
+    "ds-label-01-bold": bold,
+    "ds-label-01-reg": !bold,
+  });
   return (
     <details
       open={open}
-      className="focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800 ds-label-01-bold"
+      className="focus-within:outline focus-within:outline-4 focus-within:outline-offset-4 focus-within:outline-blue-800 text-blue-800"
     >
-      <summary className=" focus:outline-none cursor-pointer">{title}</summary>
+      <summary className={summaryClasses}>{title}</summary>
       <span className="block ds-label-01-reg pt-4 pl-16 text-black">
         {typeof content === "string" ? (
           <RichText markdown={content} />
