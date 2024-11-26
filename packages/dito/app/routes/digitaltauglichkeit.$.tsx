929f3751ac0b86f02065a0d24195b87bb713c278
lucas <lucas.solcher@digitalservice.bund.de>
2024-11-26 11:23:02 +0100

    Fix tests and introduce 301 for redirect

--- packages/dito/app/routes/digitaltauglichkeit.$.tsx
+++ packages/dito/app/routes/digitaltauglichkeit.$.tsx
@@ -3,5 +3,8 @@ import { LoaderFunctionArgs, redirect } from "@remix-run/node";
 export function loader({ request }: LoaderFunctionArgs) {
   // Redirect all requests to /digitaltauglichkeit/... to /beispiele/...
   const url = new URL(request.url);
-  return redirect(url.pathname.replace("/digitaltauglichkeit", "/beispiele"));
+  return redirect(
+    url.pathname.replace("/digitaltauglichkeit", "/beispiele"),
+    301,
+  );
 }
--- packages/dito/tests/playwright/a11y/general.spec.ts
+++ packages/dito/tests/playwright/a11y/general.spec.ts
@@ -8,10 +8,21 @@ test.describe("basic example a11y test", () => {
   test("check a11y of all routes", async ({ page }) => {
     for (const route of allRoutes.filter(
       (allRoute) =>
-        !allRoute.url.includes("/digitaltauglichkeit") && // TODO: include after pw is removed
+        !allRoute.url.includes("/beispiele") && // TODO: include after pw is removed
         !allRoute.url.endsWith(".pdf") &&
         !allRoute.url.endsWith(ROUTE_SUPPORT.url),
     )) {
+      // Listen for redirects and update URL if needed
+      const response = await page.goto(route.url);
+
+      if (
+        (response !== null && response.status() === 302) ||
+        (response !== null && response.status() === 301)
+      ) {
+        const redirectedUrl = response.headers()["location"];
+        await page.goto(redirectedUrl);
+      }
+
       await page.goto(route.url);
       const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
       console.log("Checking A11Y on route:", route.url);
