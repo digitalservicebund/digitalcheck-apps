1fb83f5ad3ade433f0976e2f12421158c75cd52b
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-12-03 13:53:19 +0100

    Add example buttons to principles

--- packages/dito/app/routes/methoden_.fuenf-prinzipien.tsx
+++ packages/dito/app/routes/methoden_.fuenf-prinzipien.tsx
@@ -5,21 +5,33 @@ import Header from "@digitalcheck/shared/components/Header";
 import Heading from "@digitalcheck/shared/components/Heading";
 import InfoBox from "@digitalcheck/shared/components/InfoBox";
 import { LoaderFunctionArgs } from "@remix-run/node";
-import { Link, MetaFunction, useLoaderData } from "@remix-run/react";
+import {
+  Link,
+  MetaFunction,
+  PrefetchPageLinks,
+  useLoaderData,
+} from "@remix-run/react";
 import { fivePrinciples } from "resources/content";
 import {
+  ROUTE_EXAMPLES,
   ROUTE_METHODS,
   ROUTE_METHODS_FIVE_PRINCIPLES,
+  ROUTE_PRINCIPLES,
 } from "resources/staticRoutes";
 import prependMetaTitle from "utils/metaTitle";
 import SupportBanner from "../components/SupportBanner.tsx";
+import {
+  fetchStrapiData,
+  GET_PRINZIPS_QUERY,
+  Prinzip,
+} from "../utils/strapiData.server.ts";
 import { slugify } from "../utils/utilFunctions.ts";
 
 export const meta: MetaFunction = ({ matches }) => {
   return prependMetaTitle(ROUTE_METHODS_FIVE_PRINCIPLES.title, matches);
 };
 
-export function loader({ request }: LoaderFunctionArgs) {
+export async function loader({ request }: LoaderFunctionArgs) {
   const referer = request.headers.get("referer");
   let pathname = "/";
 
@@ -27,13 +39,24 @@ export function loader({ request }: LoaderFunctionArgs) {
     pathname = new URL(referer).pathname;
   }
 
+  const prinzipData = await fetchStrapiData<{ prinzips: Prinzip[] }>(
+    GET_PRINZIPS_QUERY,
+  );
+
+  if ("error" in prinzipData) {
+    // eslint-disable-next-line @typescript-eslint/only-throw-error
+    throw new Response(prinzipData.error, { status: 400 });
+  }
+
   return {
     referrer: pathname,
+    prinzips: prinzipData.prinzips,
   };
 }
 
 export default function Index() {
-  const { referrer } = useLoaderData<typeof loader>();
+  const { referrer, prinzips } = useLoaderData<typeof loader>();
+  console.log(prinzips);
 
   const nextStep = referrer.startsWith(ROUTE_METHODS.url)
     ? fivePrinciples.nextStepMethods
@@ -54,13 +77,13 @@ export default function Index() {
           <div className="mt-64">
             <Heading tagName="div" text="Inhalt" className="font-bold" />
             <ol className="ds-stack-8 mt-16">
-              {fivePrinciples.principles.map((principal) => (
-                <li key={principal.label}>
+              {fivePrinciples.principles.map((principle) => (
+                <li key={principle.label}>
                   <Link
-                    to={`#${slugify(principal.label)}`}
+                    to={`#${slugify(principle.label)}`}
                     className="underline underline-offset-4 decoration-1"
                   >
-                    ↓ {principal.label}: {principal.title}
+                    ↓ {principle.label}: {principle.title}
                   </Link>
                 </li>
               ))}
@@ -68,28 +91,50 @@ export default function Index() {
           </div>
         </Container>
       </Background>
-      {fivePrinciples.principles.map((principal, index) => (
-        <Background
-          key={principal.content}
-          backgroundColor={index % 2 === 0 ? "white" : "blue"}
-        >
-          <div id={slugify(principal.label)} />
-          <Container>
-            <InfoBox
-              heading={{
-                tagName: "h2",
-                text: principal.title,
-              }}
-              label={{
-                tagName: "div",
-                text: principal.label,
-                className: "ds-label-section text-gray-900",
-              }}
-              items={[{ content: principal.content }]}
-            />
-          </Container>
-        </Background>
-      ))}
+      {fivePrinciples.principles.map((principle, index) => {
+        const prinzip = prinzips.find(
+          (principle) => principle.Nummer === index,
+        );
+        const buttonLink = prinzip
+          ? `${ROUTE_PRINCIPLES.url}/${prinzip?.URLBezeichnung}`
+          : ROUTE_EXAMPLES.url;
+        const label = slugify(principle.label);
+        return (
+          <Background
+            key={label}
+            backgroundColor={index % 2 === 0 ? "white" : "blue"}
+          >
+            <div id={label} />
+            <Container>
+              <InfoBox
+                heading={{
+                  tagName: "h2",
+                  text: principle.title,
+                }}
+                label={{
+                  tagName: "div",
+                  text: principle.label,
+                  className: "ds-label-section text-gray-900",
+                }}
+                items={[
+                  {
+                    content: principle.content,
+                    buttons: [
+                      {
+                        text: "Beispiele betrachten",
+                        href: buttonLink,
+                        look: "tertiary" as const,
+                      },
+                    ],
+                  },
+                ]}
+              />
+              {/* The button prop does not support prefetching, so we are using the PrefetchPageLinks component instead */}
+              <PrefetchPageLinks page={buttonLink} />
+            </Container>
+          </Background>
+        );
+      })}
       <Container>
         <Box
           heading={{ text: nextStep.title }}
