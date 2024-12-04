e8c6d4a32401c3918fe8b8483822191b5fb2315b
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-12-04 11:22:30 +0100

    Add items without label to InlineInfoList

--- packages/shared/components/InlineInfoList.tsx
+++ packages/shared/components/InlineInfoList.tsx
@@ -1,8 +1,12 @@
 import classNames from "classnames";
 import type { ReactNode } from "react";
 
+export type InlineInfoListItem =
+  | { key?: string; label: string; value: ReactNode }
+  | { key: string; label?: string; value: ReactNode };
+
 export type InlineInfoListProps = {
-  items: { label: string; value: ReactNode }[];
+  items: InlineInfoListItem[];
   className?: string;
 };
 
@@ -11,12 +15,15 @@ export default function InlineInfoList({
   className,
 }: InlineInfoListProps) {
   return (
-    <div className={classNames("bg-blue-200 pl-16 space-x-16", className)}>
+    <div className={classNames("bg-blue-200 space-x-16", className)}>
       {items.map(
         (item) =>
           item.value && (
-            <p key={item.label} className="inline-flex items-center space-x-8">
-              <span>{item.label}:</span>
+            <p
+              key={item.key ?? item.label}
+              className="inline-flex items-center space-x-8"
+            >
+              {item.label && <span>{item.label}:</span>}
               <span className="ds-label-01-bold">{item.value}</span>
             </p>
           ),
