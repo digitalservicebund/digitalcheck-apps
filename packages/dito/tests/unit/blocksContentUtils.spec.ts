b3d7d21cf9a4ae84fdbb8b3b14e902cb7f2a62b1
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2024-12-09 16:53:01 +0100

    Add test for prependNumberToAbsatz

--- packages/dito/tests/unit/blocksContentUtils.spec.ts
+++ packages/dito/tests/unit/blocksContentUtils.spec.ts
@@ -1,7 +1,51 @@
-import { nestListInListItems } from "utils/blocksContentUtils";
+import {
+  AbsatzWithNumber,
+  nestListInListItems,
+  prependNumberToAbsatz,
+} from "utils/blocksContentUtils";
 import { describe, expect, it } from "vitest";
 
-describe("ParagraphList nestListInListItems", () => {
+describe("prependNumberToAbsatz", () => {
+  it("should prepend number to first text node in absatz", () => {
+    const textNode1 = { type: "text" as const, text: "TEXT1" };
+    const textNode2 = { type: "text" as const, text: "TEXT2" };
+    const absatz: AbsatzWithNumber = {
+      id: 1,
+      number: 42,
+      Text: [
+        {
+          type: "paragraph",
+          children: [textNode1, textNode2],
+        },
+        {
+          type: "paragraph",
+          children: [textNode1, textNode2],
+        },
+      ],
+      PrinzipErfuellungen: [],
+    };
+
+    const result = prependNumberToAbsatz(absatz);
+    expect(result).toEqual([
+      {
+        type: "paragraph",
+        children: [
+          { type: "text", text: "(42) TEXT1" },
+          { type: "text", text: "TEXT2" },
+        ],
+      },
+      {
+        type: "paragraph",
+        children: [
+          { type: "text", text: "TEXT1" },
+          { type: "text", text: "TEXT2" },
+        ],
+      },
+    ]);
+  });
+});
+
+describe("nestListInListItems", () => {
   it("should nest lists within list items correctly", () => {
     const textNode = { type: "text", text: "TEXT" };
     const input = [
