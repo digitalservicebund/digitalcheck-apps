2b4d61e5dccca04f7402b30c630c21e9b2e996e7
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2024-12-17 15:30:25 +0100

    Adapt result header including interop

--- packages/dito/app/routes/vorpruefung.ergebnis/ResultPositive.tsx
+++ packages/dito/app/routes/vorpruefung.ergebnis/ResultPositive.tsx
@@ -5,13 +5,16 @@ import { PreCheckAnswers } from "routes/vorpruefung.$questionId/route";
 import getReasoningText from "./getReasoningText";
 import ResultForm from "./ResultForm";
 import ResultHeader from "./ResultHeader";
+import { ResultType, TResult } from "./TResult.tsx";
 
 const { title, reasoningIntro, nextSteps } = preCheck.result.positive;
 
 export default function ResultPositive({
   answers,
+  result,
 }: Readonly<{
   answers: PreCheckAnswers;
+  result: TResult;
 }>) {
   const positiveQuestions = Object.keys(answers).filter(
     (key) => answers[key] === "yes",
@@ -23,11 +26,20 @@ export default function ResultPositive({
     "positiveResult",
   );
 
+  let titleInteroperability: string = "";
+  if (result.interoperability === ResultType.POSITIVE) {
+    titleInteroperability = preCheck.result.interoperability.positive.title;
+  } else if (result.interoperability === ResultType.NEGATIVE) {
+    titleInteroperability = preCheck.result.interoperability.negative.title;
+  } else {
+    titleInteroperability = preCheck.result.interoperability.unsure.title;
+  }
+
   return (
     <>
       <ResultHeader
         resultType="positive"
-        resultHeading={title}
+        resultHeading={title + titleInteroperability}
         reasonsText={reasonsText}
         resultBackgroundColor="midBlue"
       >
