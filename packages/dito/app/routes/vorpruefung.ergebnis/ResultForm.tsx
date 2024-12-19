0c8adf46accbcc4568c531dc8245df5d08edcbd7
Merlin Pannewitz <merlin.pannewitz@digitalservice.bund.de>
2024-12-19 08:45:35 +0100

    Remove quicksent feature flag code

--- packages/dito/app/routes/vorpruefung.ergebnis/ResultForm.tsx
+++ packages/dito/app/routes/vorpruefung.ergebnis/ResultForm.tsx
@@ -9,15 +9,12 @@ import React, { useState } from "react";
 import { preCheck } from "resources/content";
 import { PreCheckAnswers } from "routes/vorpruefung.$questionId/route";
 import getResultValidatorForAnswers from "routes/vorpruefung.ergebnis/resultValidation";
-import { useFeatureFlag } from "utils/featureFlags";
 
 export default function ResultForm({
   answers,
 }: Readonly<{
   answers: PreCheckAnswers;
 }>) {
-  const quickSendNkrFlag = useFeatureFlag("digitalcheck.quicksend-nkr");
-
   const form = useForm({
     validator: getResultValidatorForAnswers(answers),
     method: "post",
@@ -42,24 +39,15 @@ export default function ResultForm({
       <form {...form.getFormProps()}>
         <fieldset className="ds-stack-32">
           <legend>
-            {quickSendNkrFlag ? (
-              <Heading tagName="h2" text={preCheck.result.form.formLegend} />
-            ) : (
-              <Heading
-                tagName="h2"
-                text="Vorprüfung ergänzen und herunterladen"
-              />
-            )}
+            <Heading tagName="h2" text={preCheck.result.form.formLegend} />
           </legend>
-          {quickSendNkrFlag && (
-            <RichText
-              markdown={
-                isPositive
-                  ? preCheck.result.form.instructionsPositive
-                  : preCheck.result.form.instructionsNegative
-              }
-            />
-          )}
+          <RichText
+            markdown={
+              isPositive
+                ? preCheck.result.form.instructionsPositive
+                : preCheck.result.form.instructionsNegative
+            }
+          />
           {Object.keys(answers).map((answer) => (
             <input
               key={answer}
@@ -83,54 +71,40 @@ export default function ResultForm({
             />
           )}
           <ButtonContainer
-            buttons={
-              quickSendNkrFlag
-                ? [
-                    {
-                      id: "result-email-button",
-                      name: "_action",
-                      value: "email",
-                      text: preCheck.result.form.sendEmailButton.text,
-                      look: "primary",
-                      className: "plausible-event-name=Quicksend+Click",
-                    },
-                    {
-                      id: "result-download-button",
-                      name: "_action",
-                      value: "download",
-                      text: preCheck.result.form.downloadPdfButton.text,
-                      look: "ghost",
-                    },
-                  ]
-                : [
-                    {
-                      id: "result-download-button",
-                      name: "_action",
-                      value: "download",
-                      text: preCheck.result.form.downloadPdfButton.text,
-                      look: "primary",
-                    },
-                  ]
-            }
+            buttons={[
+              {
+                id: "result-email-button",
+                name: "_action",
+                value: "email",
+                text: preCheck.result.form.sendEmailButton.text,
+                look: "primary",
+                className: "plausible-event-name=Quicksend+Click",
+              },
+              {
+                id: "result-download-button",
+                name: "_action",
+                value: "download",
+                text: preCheck.result.form.downloadPdfButton.text,
+                look: "ghost",
+              },
+            ]}
           />
         </fieldset>
       </form>
-      {quickSendNkrFlag && (
-        <div className="ds-stack-16 mt-40">
-          <Heading
-            tagName="h3"
-            className="ds-label-section"
-            text={preCheck.result.form.faqs.title}
+      <div className="ds-stack-16 mt-40">
+        <Heading
+          tagName="h3"
+          className="ds-label-section"
+          text={preCheck.result.form.faqs.title}
+        />
+        {preCheck.result.form.faqs.details.map((detail) => (
+          <DetailsSummary
+            key={detail.label}
+            title={detail.label}
+            content={detail.text}
           />
-          {preCheck.result.form.faqs.details.map((detail) => (
-            <DetailsSummary
-              key={detail.label}
-              title={detail.label}
-              content={detail.text}
-            />
-          ))}
-        </div>
-      )}
+        ))}
+      </div>
     </>
   );
 }
