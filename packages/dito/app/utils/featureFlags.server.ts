14ea3d4d939b0aeaef0060251555d1196ad75509
lucas <lucas.solcher@digitalservice.bund.de>
2024-12-20 10:19:06 +0100

    Prevent unleash logging on ci

--- packages/dito/app/utils/featureFlags.server.ts
+++ packages/dito/app/utils/featureFlags.server.ts
@@ -1,6 +1,16 @@
 import { initialize } from "unleash-client";
 import { UNLEASH_API_URL, UNLEASH_APP, UNLEASH_KEY } from "./constants.server";
 
+const isCI = process.env.CI === "true";
+
+const silentLogger = {
+  log: () => {},
+  error: () => {},
+  warn: () => {},
+};
+
+const logger = isCI ? silentLogger : console;
+
 const unleash = initialize({
   url: UNLEASH_API_URL,
   appName: UNLEASH_APP,
@@ -8,20 +18,21 @@ const unleash = initialize({
     Authorization: UNLEASH_KEY,
   },
 });
+
 const DEFAULT_FLAG_STATE = true;
 let unleashInitialized = false;
 
-unleash.on("ready", console.log);
+unleash.on("ready", logger.log);
 unleash.on("synchronized", () => {
   unleashInitialized = true;
-  console.log("Unleash is initialized.");
+  logger.log("Unleash is initialized.");
 });
-unleash.on("error", console.error);
-unleash.on("warn", console.warn);
+unleash.on("error", logger.error);
+unleash.on("warn", logger.warn);
 
 export const getFeatureFlag = (name: string): boolean => {
   if (!unleashInitialized) {
-    console.warn(
+    logger.warn(
       `Unleash not initialized. Defaulting "${name}" to ${DEFAULT_FLAG_STATE}.`,
     );
     return DEFAULT_FLAG_STATE;
