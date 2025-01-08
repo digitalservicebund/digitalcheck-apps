0409b20f7ef1b0db867b84137bebfe69c12e158b
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-01-08 12:37:19 +0100

    Run playwright on separate port

--- packages/dito/tests/playwright.config.ts
+++ packages/dito/tests/playwright.config.ts
@@ -11,10 +11,9 @@ dotenv.config({ path: path.resolve(__dirname, "../", ".env.test") });
 const config: PlaywrightTestConfig = {
   ...baseConfig,
   webServer: {
-    command: "npm run dev",
-    port: 5173,
+    command: "npm run dev -- --port 5172",
+    port: 5172,
     timeout: parseInt(process.env.WAIT_ON_TIMEOUT ?? `${5 * 1000}`),
-    reuseExistingServer: true,
   },
 };
 
