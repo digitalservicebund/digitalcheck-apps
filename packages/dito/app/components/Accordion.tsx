7ab63d06a007446e027fe18e726ae49e2be499c2
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-01-09 11:59:55 +0100

    Don't use index as key

--- packages/dito/app/components/Accordion.tsx
+++ packages/dito/app/components/Accordion.tsx
@@ -3,9 +3,9 @@ import AccordionItem, { AccordionItemProps } from "./AccordionItem";
 export default function Accordion({ items }: { items: AccordionItemProps[] }) {
   return (
     <div className="accordion bg-white border-b-2 border-b-blue-800">
-      {items.map((item, index) => (
+      {items.map((item) => (
         <AccordionItem
-          key={index}
+          key={item.headline}
           headline={item.headline}
           content={item.content}
           id={item.id}
