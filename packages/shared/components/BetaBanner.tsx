3d953fc0689cc9239d127c321bf80784bae0868d
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-01-13 10:35:31 +0100

    Change tool finder banner wording

--- packages/shared/components/BetaBanner.tsx
+++ packages/shared/components/BetaBanner.tsx
@@ -7,7 +7,7 @@ const BetaBanner = () => {
     <Background backgroundColor="yellow" className="pt-16 pb-16">
       <Container className="pt-0 pb-0">
         <RichText
-          markdown={`Der Werkzeugfinder befindet sich im Aufbau und wird auf Basis Ihrer Rückmeldung weiterentwickelt. Inhalte werden regelmäßig angepasst und die Funktionalität kann zeitweise eingeschränkt sein.`}
+          markdown={`Der Werkzeugfinder wird aktuell nicht weiterentwickelt. Dargestellte Informationen sind möglicherweise überholt.`}
         />
       </Container>
     </Background>
