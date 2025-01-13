a5b01840260bfaa88649caf4248aee466fcedf3f
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-01-13 10:44:15 +0100

    Make props readonly

--- packages/dito/app/components/Accordion.tsx
+++ packages/dito/app/components/Accordion.tsx
@@ -1,6 +1,8 @@
 import AccordionItem, { AccordionItemProps } from "./AccordionItem";
 
-export default function Accordion({ items }: { items: AccordionItemProps[] }) {
+export default function Accordion({
+  items,
+}: Readonly<{ items: AccordionItemProps[] }>) {
   return (
     <div className="accordion bg-white border-b-2 border-b-blue-800">
       {items.map((item, index) => (
