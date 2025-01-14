533798483084508a8721377e05f2b1f11c51b048
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-01-14 17:30:15 +0100

    Split a11y check into separate tests

--- packages/dito/tests/a11y/general.spec.ts
+++ packages/dito/tests/a11y/general.spec.ts
@@ -5,29 +5,31 @@ import allRoutes from "resources/allRoutes";
 import { ROUTE_PRINCIPLES, ROUTE_SUPPORT } from "resources/staticRoutes";
 
 test.describe("basic example a11y test", () => {
-  test.setTimeout(60000);
-  test("check a11y of all routes", async ({ page }) => {
-    for (const route of allRoutes.filter(
+  allRoutes
+    .filter(
       (allRoute) =>
         !allRoute.url.endsWith(".pdf") &&
         !allRoute.url.endsWith(ROUTE_SUPPORT.url),
-    )) {
-      // Listen for redirects and update URL if needed
-      const response = await page.goto(route.url);
+    )
+    .forEach((route) => {
+      test(`check a11y of ${route.title}`, async ({ page }) => {
+        // Listen for redirects and update URL if needed
+        const response = await page.goto(route.url);
 
-      if (
-        (response !== null && response.status() === 302) ||
-        (response !== null && response.status() === 301)
-      ) {
-        const redirectedUrl = response.headers()["location"];
-        await page.goto(redirectedUrl);
-      }
+        if (
+          (response !== null && response.status() === 302) ||
+          (response !== null && response.status() === 301)
+        ) {
+          const redirectedUrl = response.headers()["location"];
+          await page.goto(redirectedUrl);
+        }
 
-      const accessibilityScanResults = await new AxeBuilder({ page }).analyze();
-      console.log("Checking A11Y on route:", route.url);
-      expect(accessibilityScanResults.violations).toEqual([]);
-    }
-  });
+        const accessibilityScanResults = await new AxeBuilder({
+          page,
+        }).analyze();
+        expect(accessibilityScanResults.violations).toEqual([]);
+      });
+    });
 
   test("check a11y of example pages", async ({ page }) => {
     await page.goto(
