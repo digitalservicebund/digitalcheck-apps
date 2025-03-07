1ee44943468305c3ce6a165c8323bb668ab02849
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-07 12:51:17 +0100

    Enable v3_throwAbortReason in Remix configuration

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,7 @@ export default defineConfig({
           unstable_optimizeDeps: true,
           v3_fetcherPersist: true,
           v3_relativeSplatPath: true,
+          v3_throwAbortReason: true,
         },
       }),
     tsconfigPaths(),
