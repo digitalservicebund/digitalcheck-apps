dee0dcf15c246d141ab2fc3259d7dd8ce811ba8a
Hendrik Schmidt <hendrik.schmidt@digitalservice.bund.de>
2025-03-10 09:22:51 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,28 +2,8 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-declare module "@remix-run/node" {
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
-
 export default defineConfig({
-  plugins: [
-    !process.env.VITEST &&
-      reactRouter({
-        future: {
-          unstable_optimizeDeps: true,
-          v3_fetcherPersist: true,
-          v3_relativeSplatPath: true,
-          v3_throwAbortReason: true,
-          v3_lazyRouteDiscovery: true,
-          v3_singleFetch: true,
-          v3_routeConfig: true,
-        },
-      }),
-    tsconfigPaths(),
-  ],
+  plugins: [!process.env.VITEST && reactRouter(), tsconfigPaths()],
   build: {
     target: "ES2022",
   },
